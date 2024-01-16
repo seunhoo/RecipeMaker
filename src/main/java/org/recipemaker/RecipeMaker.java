e6bafbbee26930a30f9f0b492634f45d6447970f
seunhoo <seunhoo@naver.com>
2024-01-16 14:04:48 +0900

    [feat] 최초 커밋

---
+++ src/main/java/org/recipemaker/RecipeMaker.java
@@ -0,0 +1,17 @@
+package org.recipemaker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RecipeMaker extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
