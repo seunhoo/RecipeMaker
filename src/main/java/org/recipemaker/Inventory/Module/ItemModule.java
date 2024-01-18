13ee0122d68ffca8570ecfc108190b805e8bd39e
seunhoo <seunhoo@naver.com>
2024-01-18 09:42:28 +0900

    Update ItemModule.java

--- src/main/java/org/recipemaker/Inventory/Module/ItemModule.java
+++ src/main/java/org/recipemaker/Inventory/Module/ItemModule.java
@@ -7,10 +7,12 @@ import org.bukkit.inventory.meta.ItemMeta;
 public class ItemModule {
     public ItemStack setItem(Material material, int amount, String title) {
         ItemStack prevItem = new ItemStack(material, amount);
+        if(material == Material.AIR){prevItem = new ItemStack(Material.ITEM_FRAME, amount);}
         ItemMeta prevItemMeta = prevItem.getItemMeta();
         assert prevItemMeta != null;
         prevItemMeta.setDisplayName(title);
         prevItem.setItemMeta(prevItemMeta);
+        if(material == Material.AIR){prevItem.setType(Material.AIR);}
         return prevItem;
     }
 }
