491795a5a6b3f21c0814bbf78a4f0dde014845cd
seunhoo <seunhoo@naver.com>
2024-01-18 11:54:41 +0900

    Update InventoryModule.java

--- src/main/java/org/recipemaker/Inventory/Module/InventoryModule.java
+++ src/main/java/org/recipemaker/Inventory/Module/InventoryModule.java
@@ -74,7 +74,7 @@ public class InventoryModule {
                     lineRecipe.append(" ");
                 }
             }
-            recipeShape[i / 9 - 1] = "\""+ String.valueOf(lineRecipe) +"\",";
+            recipeShape[i / x - 1] = "\""+ String.valueOf(lineRecipe) +"\",";
         }
 
         recipeShape[2] = recipeShape[2].substring(0, recipeShape[2].length() - 1);
